62a56aff271216422a043929c0af461d61b0c9db
Xonical <sebastian-schoen@outlook.com>
2014-09-10 10:52:56 +0200

    Add class

---
+++ GameViewAreas.cpp
@@ -0,0 +1,28 @@
+#include "GameViewAreas.h"
+
+int TopbarArea::x;
+
+TopbarArea::TopbarArea()
+{
+}
+
+TopbarArea::~TopbarArea()
+{
+
+}
+
+GameArea::GameArea()
+{
+}
+
+GameArea::~GameArea()
+{
+}
+
+CharactersArea::CharactersArea()
+{
+}
+
+CharactersArea::~CharactersArea()
+{
+}
---
+++ GameViewAreas.h
@@ -0,0 +1,37 @@
+#pragma once
+class TopbarArea
+{
+public:
+	TopbarArea();
+	~TopbarArea();
+	static int TopbarArea::x;
+	static int TopbarArea::y;
+	static int TopbarArea::Width;
+	static int TopbarArea::Height;
+};
+
+class GameArea
+{
+public:
+	GameArea();
+	~GameArea();
+	static int GameArea::x;
+	static int GameArea::y;
+	static int GameArea::Width;
+	static int GameArea::Height;
+};
+
+
+class CharactersArea
+{
+public:
+	CharactersArea();
+	~CharactersArea();
+	static int CharactersArea::x;
+	static int CharactersArea::y;
+	static int CharactersArea::Width;
+	static int CharactersArea::Height;
+};
+
+
+
